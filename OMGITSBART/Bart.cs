39329ffd4dd961e0e5641fc1d5c80ab2b5aa8caf
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-11-19 19:18:23 -0300

    Add project files.

---
+++ OMGITSBART/Bart.cs
@@ -0,0 +1,62 @@
+﻿using MTM101BaldAPI.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace OMGITSBART
+{
+    public class Bart : NPC
+    {
+        public Sprite bartSprite;
+        public SoundObject simpsonGamer;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            spriteRenderer[0].sprite = bartSprite;
+
+            AudioManager audioManager = GetComponent<AudioManager>();
+            PropagatedAudioManager simpsonGamerPlayer = base.gameObject.AddComponent<PropagatedAudioManager>();
+            simpsonGamerPlayer.audioDevice = base.gameObject.AddComponent<AudioSource>();
+            simpsonGamerPlayer.ReflectionSetVariable("soundOnStart", new SoundObject[] { simpsonGamer });
+            simpsonGamerPlayer.ReflectionSetVariable("loopOnStart", true);
+
+            behaviorStateMachine.ChangeState(new Bart_Wander(this));
+        }
+    }
+
+    public class Bart_StateBase : NpcState
+    {
+        protected Bart SimpsonGamer;
+
+        public Bart_StateBase(Bart simpsongame) : base(simpsongame)
+        {
+            SimpsonGamer = simpsongame;
+        }
+    }
+
+    public class Bart_Wander : Bart_StateBase
+    {
+        public Bart_Wander(Bart simpsongame) : base(simpsongame)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            if (!npc.Navigator.HasDestination)
+            {
+                ChangeNavigationState(new NavigationState_WanderRandom(npc, 0));
+            }
+        }
+
+        public override void DestinationEmpty()
+        {
+            base.DestinationEmpty();
+            ChangeNavigationState(new NavigationState_WanderRandom(npc, 0));
+        }
+    }
+}
---
+++ OMGITSBART/BasePlugin.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
+using MTM101BaldAPI.ObjectCreation;
+using MTM101BaldAPI.Registers;
+using System.Collections;
+using UnityEngine;
+
+namespace OMGITSBART
+{
+    [BepInPlugin("starrie.bbplus.omgitsbart", "Bart In BB+", "2.0.0.0")]
+
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi")]
+
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public AssetManager assetMan = new AssetManager();
+        private static string npcSubDirectory = "Bart";
+
+        private IEnumerator RegisterAssets()
+        {
+            yield return 1;
+
+            yield return "Building NPCs...";
+
+            Bart SimpsonGamer = new NPCBuilder<Bart>(Info)
+              .SetName("Bart")
+              .SetEnum("SimpsonGamer")
+              .SetMinMaxAudioDistance(1, 300)
+              .IgnorePlayerOnSpawn()
+              .AddSpawnableRoomCategories(new RoomCategory[] { RoomCategory.Hall, RoomCategory.Class, RoomCategory.Faculty })
+              .SetPoster(assetMan.Get<Texture2D>("BartPoster"), "Bart", "Omg its bart wait a minute? What the fuck bart!!!")
+              .Build();
+
+            yield return "Doing some miscellaneous stuff...";
+
+            SimpsonGamer.bartSprite = assetMan.Get<Sprite>("BartSprite");
+            SimpsonGamer.simpsonGamer = assetMan.Get<SoundObject>("SimpsonGamer");
+
+            assetMan.Add<NPC>("Bart", SimpsonGamer);
+
+            yield break;
+        }
+
+        private void GetAssets()
+        {
+            assetMan.Add<Texture2D>("Bart", AssetLoader.TextureFromMod(this, npcSubDirectory, "Me.png"));
+            assetMan.Add<Texture2D>("BartPoster", AssetLoader.TextureFromMod(this, npcSubDirectory, "POS_me.png"));
+            assetMan.Add<Sprite>("BartSprite", AssetLoader.SpriteFromTexture2D(assetMan.Get<Texture2D>("Bart"), 30));
+            assetMan.Add<SoundObject>("SimpsonGamer", ObjectCreators.CreateSoundObject(AssetLoader.AudioClipFromMod(this, npcSubDirectory, "Sound/Simpson_Gamer.ogg"), "*SIMPSON GAMER*", SoundType.Music, Color.yellow));
+        }
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("starrie.bbplus.omgitsbart");
+
+            harmony.PatchAll();
+
+            GetAssets();
+
+            LoadingEvents.RegisterOnAssetsLoaded(Info, RegisterAssets(), LoadingEventOrder.Pre);
+            GeneratorManagement.Register(this, GenerationModType.Addend, AddObjects);
+        }
+
+        private void AddObjects(string floor, int floorNumber, SceneObject floorObject)
+        {
+            if (floor.StartsWith("F"))
+            {
+                floorObject.potentialNPCs.Add(new WeightedNPC()
+                {
+                    selection = assetMan.Get<NPC>("Bart"),
+                    weight = floorNumber < 2 ? 200 * floorNumber : 450
+                }
+                );
+            }
+            else if (floor == "END")
+            {
+                floorObject.potentialNPCs.Add(new WeightedNPC()
+                {
+                    selection = assetMan.Get<NPC>("Bart"),
+                    weight = 450
+                }
+                );
+            }
+
+            floorObject.MarkAsNeverUnload();
+        }
+    }
+}
